b826c26ff3b40a395aff5e7fd0d4f223a5795efa
Oran Luzon <oran.luzon@gmail.com>
2015-09-17 23:57:52 -0400

    fixed Yolis silly instruction code from 60 -> 10 lines

--- AtomDashSwift/InstructionsScene.swift
+++ AtomDashSwift/InstructionsScene.swift
@@ -37,42 +37,16 @@ class InstructionsScene: SKScene {
         
         targetNode = Target()
         targetNode!.position = CGPoint(x: (3*self.frame.width)/4, y: (4*self.frame.height)/5)
+
+        lineOnePartOne = makeLabel("You are the", position: CGPoint(x: self.frame.width/2.7, y: (3*self.frame.height)/5), color: UIColor(red: 0, green: 0, blue: 0, alpha: 1))
+        lineOnePartTwo = makeLabel("BLUE", position: CGPoint(x: self.frame.midX * 1.5, y: (3*self.frame.height)/5), color: UIColor(red: 0.62, green: 0.85, blue: 0.94, alpha: 1))
         
-        lineOnePartOne = SKLabelNode(text: "You are the")
-        lineOnePartOne!.position = CGPoint(x: self.frame.width/2.7, y: (3*self.frame.height)/5)
-        lineOnePartOne!.fontName = "DINCondensed-Bold"
-        lineOnePartOne!.fontSize = 50
-        lineOnePartOne!.fontColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        lineTwoPartOne = makeLabel("Run from the", position: CGPoint(x: self.frame.width/2.5, y: (2.5*self.frame.height)/5), color: UIColor(red: 0, green: 0, blue: 0, alpha: 1))
+        lineTwoPartTwo = makeLabel("RED", position: CGPoint(x: lineOnePartOne!.position.x + (lineOnePartTwo!.frame.width * 1.9), y: (2.5*self.frame.height)/5), color: UIColor(red: 0.94, green: 0.55, blue: 0.55, alpha: 1))
         
-        lineOnePartTwo = SKLabelNode(text: "BLUE")
-        lineOnePartTwo!.position = CGPoint(x: lineOnePartOne!.position.x + (lineOnePartTwo!.frame.width*1.8), y: (3*self.frame.height)/5)
-        lineOnePartTwo!.fontName = "DINCondensed-Bold"
-        lineOnePartTwo!.fontSize = 50
-        lineOnePartTwo!.fontColor = UIColor(red: 0.62, green: 0.85, blue: 0.94, alpha: 1)
-        
-        lineTwoPartOne = SKLabelNode(text: "Run from the")
-        lineTwoPartOne!.position = CGPoint(x: self.frame.width/2.5, y: (2.5*self.frame.height)/5)
-        lineTwoPartOne!.fontName = "DINCondensed-Bold"
-        lineTwoPartOne!.fontSize = 50
-        lineTwoPartOne!.fontColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
-        
-        lineTwoPartTwo = SKLabelNode(text: "RED")
-        lineTwoPartTwo!.position = CGPoint(x: lineOnePartOne!.position.x + (lineOnePartTwo!.frame.width * 1.9), y: (2.5*self.frame.height)/5)
-        lineTwoPartTwo!.fontName = "DINCondensed-Bold"
-        lineTwoPartTwo!.fontSize = 50
-        lineTwoPartTwo!.fontColor = UIColor(red: 0.94, green: 0.55, blue: 0.55, alpha: 1)
-        
-        lineThreePartOne = SKLabelNode(text: "Get the")
-        lineThreePartOne!.position = CGPoint(x: self.frame.width/3, y: (2*self.frame.height)/5)
-        lineThreePartOne!.fontName = "DINCondensed-Bold"
-        lineThreePartOne!.fontSize = 50
-        lineThreePartOne!.fontColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
-        
-        lineThreePartTwo = SKLabelNode(text: "GREEN")
-        lineThreePartTwo!.position = CGPoint(x: lineOnePartOne!.position.x + (lineOnePartTwo!.frame.width * 1.25), y: (2*self.frame.height)/5)
-        lineThreePartTwo!.fontName = "DINCondensed-Bold"
-        lineThreePartTwo!.fontSize = 50
-        lineThreePartTwo!.fontColor = UIColor(red: 0.59, green: 0.89, blue: 0.56, alpha: 1)
+        lineThreePartOne = makeLabel("Get the", position: CGPoint(x: self.frame.width/3, y: (2*self.frame.height)/5), color: UIColor(red: 0, green: 0, blue: 0, alpha: 1))
+        lineThreePartTwo = makeLabel("GREEN", position: CGPoint(x: lineOnePartOne!.position.x + (lineOnePartTwo!.frame.width * 1.25), y: (2*self.frame.height)/5), color: UIColor(red: 0.59, green: 0.89, blue: 0.56, alpha: 1))
+
         
         menuButton = ButtonTemplate(name: "MenuButton",labelName: "MENU",  size: CGSize(width: self.frame.width/2.5, height: self.frame.width/8), position: CGPoint(x: self.frame.midX, y: (self.frame.height)/5), color: UIColor(red: 0.59, green: 0.89, blue: 0.56, alpha: 1))
         
@@ -104,5 +78,14 @@ class InstructionsScene: SKScene {
             }
         }
     }
-    
+    func makeLabel(text: String, position: CGPoint, color: UIColor) -> SKLabelNode{
+        var newLabel = SKLabelNode(text: text)
+        newLabel.position = position
+        newLabel.fontName = "DINCondensed-Bold"
+        newLabel.fontSize = 50
+        newLabel.fontColor = color
+        
+        return newLabel
+    }
+
 }
