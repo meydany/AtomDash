c5c6ce1d32a5c21197f752a057c3a366b226a8d2
Yoli Meydan <meydany@bxscience.edu>
2015-10-31 23:17:32 -0400

    Added coin instructions

--- AtomDashSwift/SingleplayerInstructionsScene.swift
+++ AtomDashSwift/SingleplayerInstructionsScene.swift
@@ -14,6 +14,7 @@ class SingleplayerInstructionsScene: SKScene, UIScrollViewDelegate{
     var firstLabelView: UITextView!
     var secondLabelView: UITextView!
     var thirdLabelView: UITextView!
+    var fourthLabelView: UITextView!
     
     var gotItButton: UIButton!
     var buttonView: UIView!
@@ -48,11 +49,11 @@ class SingleplayerInstructionsScene: SKScene, UIScrollViewDelegate{
         scrollView.directionalLockEnabled = true
         scrollView.pagingEnabled = true
         scrollView.indicatorStyle = UIScrollViewIndicatorStyle.White
-        scrollView.contentSize = CGSize(width: view.frame.width * 3, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * 4, height: view.frame.height)
         scrollView.alpha = 0
         
         pageControl = UIPageControl(frame: CGRectMake((3*view.frame.width)/8,view.frame.height/1.25,scrollView.frame.width/4, scrollView.frame.height/8))
-        pageControl.numberOfPages = 3
+        pageControl.numberOfPages = 4
         pageControl.currentPage = 0
         pageControl.tintColor = UIColor.redColor()
         pageControl.pageIndicatorTintColor = UIColor.lightGrayColor()
@@ -65,11 +66,11 @@ class SingleplayerInstructionsScene: SKScene, UIScrollViewDelegate{
         firstLabelView = makeTextView("You are the ", part2: "BLUE", color: UIColor.gameBlueColor())
         secondLabelView = makeTextView("Avoid the ", part2: "RED", color: UIColor.gameRedColor())
         thirdLabelView = makeTextView("Get the ", part2: "GREEN", color: UIColor.gameGreenColor())
-
+        fourthLabelView = makeTextView("Collect the ", part2: "COINS", color: UIColor.gameGoldColor())
 
         //Button frame
         gotItButton = UIButton(frame: CGRect(x: 0, y: 0, width: self.frame.width/2.5, height: self.frame.width/7))
-        gotItButton.frame.origin = CGPoint(x: (self.frame.midX)*5 - gotItButton.frame.width/2, y: self.frame.height/1.5)
+        gotItButton.frame.origin = CGPoint(x: (self.frame.midX)*7 - gotItButton.frame.width/2, y: self.frame.height/1.5)
         gotItButton.layer.cornerRadius = 10
         gotItButton.backgroundColor = UIColor(red: 0.59, green: 0.89, blue: 0.56, alpha: 1)
         gotItButton.setTitle("GOT IT", forState: UIControlState.Normal)
@@ -85,6 +86,7 @@ class SingleplayerInstructionsScene: SKScene, UIScrollViewDelegate{
         scrollView.addSubview(firstLabelView)
         scrollView.addSubview(secondLabelView)
         scrollView.addSubview(thirdLabelView)
+        scrollView.addSubview(fourthLabelView)
         scrollView.addSubview(gotItButton)
         
         self.view?.addSubview(scrollView)
